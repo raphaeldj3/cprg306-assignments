7d9169fc571ca1ac1f2d750cb109d3bf54e3aee2
raphaeldj3 <raphael.t.dejesus@edu.sait.ca>
2025-05-13 10:00:56 -0600

    Set up for Week 2 Assignment + Progress on Week 2 Assignment
    This commit contains setting up for the week 2 assignment plus progress on the week 2 assignment.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return(
+        <div>
+            <p>Raphael de Jesus</p>
+            <Link href="https://github.com/raphaeldj3/cprg306-assignments">My Github Repository</Link>
+        </div>
+    );
+}
